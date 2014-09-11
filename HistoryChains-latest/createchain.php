20a1778225aa529cd212dda88522f7677e5b8e0c
elliotjr <elliotjamesrandall@gmail.com>
2014-09-11 12:48:29 +1000

    Chain Created - Plugin Used
    Vertical Timline plugin used - approved by tutor

--- HistoryChains-latest/createchain.php
+++ HistoryChains-latest/createchain.php
@@ -4,6 +4,19 @@
 		<link rel="stylesheet" type="text/css" href="style.css" />
 		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		
+		<!-- This is for the timeline -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+
+		<link rel="stylesheet" href="vertical-timeline/css/style.css"> <!-- Resource style -->
+		<script src="verticle-timeline/js/modernizr.js"></script> <!-- Modernizr -->
+		<!-- This is for the timeline -->
+	
+	
 		<script>
 			$(document).ready(function(){
 				$(".CreateNew").hide(); 
@@ -18,6 +31,7 @@
 				}); 
 			});
 		</script>
+		
 	</head>
 	
 	<body>
@@ -68,16 +82,106 @@
 						<input type="text" name="SearchField" placeholder="Enter keywords, authors, public figures or events to begin your chain."/>	
 					</form>
 					<article class="SearchResults">
+						<div class="results">
+							<!--This is where the search results reside-->
+						</div>
 						<p>Start Searching so we can show you some results!</p>
 					</article>
 				</div>
 			</section>
 			
-			<section class="TopChain">
-				<h2>Your Chain</h2>
-			</section>
+					
+	<section class="TopChain">
+		<h2>Your Chain</h2>
+		<section id="cd-timeline" class="cd-container">
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-img cd-picture">
+					<img src="vertical-timeline/img/cd-icon-picture.svg" alt="Picture">
+				</div> <!-- cd-timeline-img -->
+
+				<div class="cd-timeline-content">
+					<h2>Australian Politics</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+					<a href="#0" class="cd-read-more">Read more</a>
+					<span class="cd-date">Jan 14</span>
+				</div> <!-- cd-timeline-content -->
+			</div> <!-- cd-timeline-block -->
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-img cd-movie">
+					<img src="vertical-timeline/img/cd-icon-movie.svg" alt="Movie">
+				</div> <!-- cd-timeline-img -->
+
+				<div class="cd-timeline-content">
+					<h2>Title of section 2</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
+					<a href="#0" class="cd-read-more">Read more</a>
+					<span class="cd-date">Jan 18</span>
+				</div> <!-- cd-timeline-content -->
+			</div> <!-- cd-timeline-block -->
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-img cd-picture">
+					<img src="vertical-timeline/img/cd-icon-picture.svg" alt="Picture">
+				</div> <!-- cd-timeline-img -->
+
+				<div class="cd-timeline-content">
+					<h2>Title of section 3</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
+					<a href="#0" class="cd-read-more">Read more</a>
+					<span class="cd-date">Jan 24</span>
+				</div> <!-- cd-timeline-content -->
+			</div> <!-- cd-timeline-block -->
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-img cd-location">
+					<img src="vertical-timeline/img/cd-icon-location.svg" alt="Location">
+				</div> <!-- cd-timeline-img -->
+
+				<div class="cd-timeline-content">
+					<h2>Title of section 4</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+					<a href="#0" class="cd-read-more">Read more</a>
+					<span class="cd-date">Feb 14</span>
+				</div> <!-- cd-timeline-content -->
+			</div> <!-- cd-timeline-block -->
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-img cd-location">
+					<img src="vertical-timeline/img/cd-icon-location.svg" alt="Location">
+				</div> <!-- cd-timeline-img -->
+
+				<div class="cd-timeline-content">
+					<h2>Title of section 5</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
+					<a href="#0" class="cd-read-more">Read more</a>
+					<span class="cd-date">Feb 18</span>
+				</div> <!-- cd-timeline-content -->
+			</div> <!-- cd-timeline-block -->
+
+			<div class="cd-timeline-block">
+				<div class="cd-timeline-img cd-movie">
+					<img src="vertical-timeline/img/cd-icon-movie.svg" alt="Movie">
+				</div> <!-- cd-timeline-img -->
+
+				<div class="cd-timeline-content">
+					<h2>Final Section</h2>
+					<p>This is the content of the last section</p>
+					<span class="cd-date">Feb 26</span>
+				</div> <!-- cd-timeline-content -->
+			</div> <!-- cd-timeline-block -->
+		</section> <!-- cd-timeline -->
+		<script src="verticle-timeline/js/main.js"></script> <!-- Resource jQuery -->
+				
+				
+	</section>
 			
-			<footer>
+			
+			
+			
+			
+			
+			<footer class="footer">
 				<p>Co-founders: Elliot Randall, Henry Chladil, Alek Thompson, Gary Myles and Angus Payne</p>
 				<p>All Rights Reserved</p>
 			</footer>
