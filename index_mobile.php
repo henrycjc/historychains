28f03f86b44ee542d24658788ba4aa0265b53883
anguspdpayne <anguspdpayne@gmail.com>
2014-09-15 11:06:05 +1000

    Adding in Mobile Detection
    Adding in Javascript to detect mobile devices and redirect to a new
    page.

---
+++ index_mobile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="resources/css/mobile_styles.css" />
+	</head>
+	<body>
+			<div id="content_area">
+				<div id="content_header">
+					<img id="header_image" src="resources/images/logo.png"/>
+					<h1 id="header_text">History Chains</h1>
+				</div>
+				<div id="content_body">
+				
+				</div>
+				<div id="content_footer">
+				
+				</div>
+			</div>
+	</body>
+</html>
