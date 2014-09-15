4d953efb368e97d99205b202fc29671059833d17
Henry Chladil <henry.ponco@gmail.com>
2014-09-15 17:57:43 +1000

    Moved main code to /app and updated mysql conn

---
+++ app/models/Mysql_Connection.php
@@ -0,0 +1,78 @@
+<?php
+
+class Mysql_Connection {
+
+	protected $usr;
+	protected $pass;
+	protected $host = "localhost";
+	protected $db = "HISTORY_CHAINS";
+	public $connected = FALSE;
+	public $dbConn;
+
+	public function __construct() {
+		$this->getEnv();
+		if (is_resource($this->dbConn) && 
+				get_resource_type($this->dbConn) === 'mysql link') {
+			echo "<h1>FIXME: already connected to dbConn</h1>";
+			$this->connected = TRUE;
+			return $this->dbConn;
+		}
+		
+		$this->dbConn = new mysqli($this->host, $this->usr, $this->pass, $this->db);
+		if ($this->dbConn->connect_errno) {
+   			echo "<h1>dbConn Error: ".$dbConn->connect_errno." ".$dbConn->connect_error;
+   			exit(1);
+		}
+		$this->connected = TRUE;
+		$this->setCharset();
+
+		return $this->dbConn;
+	}
+
+	public function getConn() {
+		return $this->dbConn;
+	}
+
+	public function closeConn() {
+		if (is_resource($this->dbConn)) {
+			$this->dbConn->close();
+		} else {
+			echo "<h1>HENRY FIX ME: dbConn already closed</h1>";
+			echo "dbConn: " . is_resource($this->dbConn) . "<br>";
+			echo get_resource_type($this->dbConn) . "<br>";
+			var_dump($this->dbConn);
+			echo "<h1> END FIX ME</h1>";
+		}
+		
+		$this->connected = FALSE;
+	}
+
+	private function getEnv() {
+
+		if ($_SERVER['REMOTE_ADDR'] === "::1") {
+			$this->usr = "root";
+			$this->pass = "";
+		} else {
+			$this->usr = "hchains";
+			$this->pass = "sQHSaLv69eetjefu";
+		}
+	}
+	
+	private function setCharset($charset = "utf8") {
+		$this->dbConn->set_charset("utf8");
+	}
+	
+}
+
+/* TESTS */
+
+$sqlConnection = new Mysql_Connection();
+
+var_dump($sqlConnection->getConn());
+echo "<br>";
+var_dump($sqlConnection);
+echo "<br>";
+$sqlConnection->closeConn();
+echo "<br>";
+var_dump($sqlConnection);
+
