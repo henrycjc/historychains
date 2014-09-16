38042f3b932fd00a2513e6555f33c80e68017df2
Henry Chladil <henry.ponco@gmail.com>
2014-09-16 15:23:33 +1000

    Fixed config file

--- app/configs/Global_Config.php
+++ app/configs/Global_Config.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set("error_reporting", -1);
 
-require_once("resources/vendor/kint/Kint.class.php");
-require_once("resources/models/Model.class.php");
-require_once("resources/controllers/Controller.class.php");
-require_once("resources/views/View.class.php");
+require_once("app/vendor/kint/Kint.class.php");
+require_once("app/models/Model.class.php");
+require_once("app/controllers/Controller.class.php");
+require_once("app/views/View.class.php");
