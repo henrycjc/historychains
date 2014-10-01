2ab3fd53055d8ccb13b23ed30b7717112b290fbd
henrycjc <henry.ponco@gmail.com>
2014-10-01 22:27:37 +1000

    Create chain functionality

--- app/configs/Global_Config.php
+++ app/configs/Global_Config.php
@@ -2,8 +2,10 @@
 error_reporting(E_ALL);
 ini_set("error_reporting", -1);
 
+
 require_once("app/vendor/kint/Kint.class.php");
 require_once("app/models/Mysql_Connection.class.php");
 require_once("app/models/Model.class.php");
 require_once("app/controllers/Controller.class.php");
 require_once("app/views/View.class.php");
+require_once("app/models/User.class.php");
